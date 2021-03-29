bbbfc1e418388049715edb2a0846269c7fa9adaa
Joel Ogunremi <joelogunremi@gmail.com>
2021-03-28 21:18:57 -0400

    Creates phrases for use in game

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,24 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = this.createPhrases();
+    this.activePhrase = null;
+  }
+  /**
+   * Creates phrases for use in game
+   * @return {array} An array of phrases that could be used in the game
+   */
+  createPhrases() {
+    const phrases = [
+      { phrase: "Life is like a box of chocolates" },
+      { phrase: "There is no trying" },
+      { phrase: "May the force be with you" },
+      { phrase: "You have to see the matrix for yourself" },
+      { phrase: "You talking to me" },
+    ];
+    return phrases;
+  }
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+const phrase = new Phrase();
+const game = new Game();
+
+//console.log(`Phrase - phrase: ${phrase.phrase}`);
+/*game.phrases.forEach((phrase, index) => {
+  console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+});*/
