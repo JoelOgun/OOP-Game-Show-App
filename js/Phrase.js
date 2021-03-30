2eceed1575c8487c293d2e2b2423a78d3ab950b8
Joel Ogunremi <joelogunremi@gmail.com>
2021-03-29 20:10:29 -0400

    addPhraseToDisplay method was added and test

--- js/Phrase.js
+++ js/Phrase.js
@@ -5,4 +5,23 @@ class Phrase {
   constructor(phrase) {
     this.phrase = phrase;
   }
+
+  /**
+   * Display phrase on game board
+   */
+  addPhraseToDisplay() {
+    for (let i = 0; i < this.phrase.length; i++) {
+      if (this.phrase.charAt(i) !== " ") {
+        const elementLi = document.createElement("li");
+        elementLi.setAttribute("class", `hide letter ${this.phrase.charAt(i)}`);
+        document.querySelector("div#phrase ul").appendChild(elementLi);
+        elementLi.textContent = `${this.phrase.charAt(i)}`;
+      } else {
+        const elementLi = document.createElement("li");
+        elementLi.setAttribute("class", `space`);
+        document.querySelector("div#phrase ul").appendChild(elementLi);
+        elementLi.textContent = " ";
+      }
+    }
+  }
 }
--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-const phrase = new Phrase();
+//const phrase = new Phrase();
 //const game = new Game();
 
 //console.log(`Phrase - phrase: ${phrase.phrase}`);
@@ -9,7 +9,7 @@ const phrase = new Phrase();
   console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
 });*/
 
-const logPhrase = (phrase) => {
+/* const logPhrase = (phrase) => {
   console.log(`Phrase - phrase: `, phrase.phrase);
 };
 const game = new Game();
@@ -18,3 +18,9 @@ logPhrase(game.getRandomPhrase());
 logPhrase(game.getRandomPhrase());
 logPhrase(game.getRandomPhrase());
 logPhrase(game.getRandomPhrase());
+*/
+
+const game = new Game();
+const randomPhrase = game.getRandomPhrase();
+const phrase = new Phrase(randomPhrase.phrase);
+phrase.addPhraseToDisplay();
